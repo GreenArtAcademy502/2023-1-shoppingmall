74e42f3e41ec094ee83e40185a27adf05c1d1f97
박도흠 <pirbak@daum.net>
2023-06-01 21:50:33 +0900

    Product - 세팅

--- src/main/java/com/green/shoppingmall/customer/CustomerController.java
+++ src/main/java/com/green/shoppingmall/customer/CustomerController.java
@@ -1,9 +1,16 @@
 package com.green.shoppingmall.customer;
 
+import com.green.shoppingmall.customer.model.CustomerInsDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
     private final CustomerService service;
 
@@ -12,4 +19,10 @@ public class CustomerController {
         this.service = service;
     }
 
+    @PostMapping
+    public ResponseEntity<Integer> postCustomer(@RequestBody CustomerInsDto dto) {
+        int result = service.insCustomer(dto);
+        return ResponseEntity.ok(result);
+        //return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
 }
