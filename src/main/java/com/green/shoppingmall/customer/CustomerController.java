861f88f498f61fe363a27d0f14d469e509729757
박도흠 <pirbak@daum.net>
2023-06-01 20:49:28 +0900

    Customer - setting table.sql

--- src/main/java/com/green/shoppingmall/customer/CustomerController.java
+++ src/main/java/com/green/shoppingmall/customer/CustomerController.java
@@ -1,8 +1,15 @@
 package com.green.shoppingmall.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
+    private final CustomerService service;
+
+    @Autowired
+    public CustomerController(CustomerService service) {
+        this.service = service;
+    }
 
 }
