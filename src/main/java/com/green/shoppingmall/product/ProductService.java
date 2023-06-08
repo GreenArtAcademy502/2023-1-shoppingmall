a2164d28f0c953b8fd4dc7725f4e6af47bf48c83
박도흠 <pirbak@daum.net>
2023-06-08 21:11:29 +0900

    Product - insProduct 완료

--- src/main/java/com/green/shoppingmall/product/ProductService.java
+++ src/main/java/com/green/shoppingmall/product/ProductService.java
@@ -4,11 +4,18 @@ import com.green.shoppingmall.product.model.ProductEntity;
 import com.green.shoppingmall.product.model.ProductInsDto;
 import com.green.shoppingmall.utils.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @Service
 public class ProductService {
+    @Value("${file.dir}")
+    private String fileDir;
+
     private final ProductMapper mapper;
 
     @Autowired
@@ -31,9 +38,26 @@ public class ProductService {
         entity.setMainPic(savedFileNm);
         int result = mapper.insProduct(entity);
 
+        //3. pk값 얻는다
         System.out.println("result : " + result);
         System.out.println("pk : " + entity.getIproduct());
 
-        return 0;
+        //4. D:/download/shoppingmall/product/pk값 폴더 생성
+        String targetDir = String.format("%s/product/%d", fileDir, entity.getIproduct());
+        File fileTargetDir = new File(targetDir);
+        if(!fileTargetDir.exists()) {
+            fileTargetDir.mkdirs();
+        }
+
+        //5. 이미지를 해당 폴더로 옮긴다.
+        //File fileTarget = new File(targetDir + "/" + savedFileNm);
+        File fileTarget = new File(String.format("%s/%s", targetDir, savedFileNm));
+        try {
+            img.transferTo(fileTarget);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+        return 1;
     }
 }
